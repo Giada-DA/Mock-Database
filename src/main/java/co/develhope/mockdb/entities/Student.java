1c001fbe944d57457e8c40e78f0e0c05a8e11c52
Giada Dall'Ara <giada.25700@gmail.com>
2023-05-07 17:29:00 +0200

    Esercizio sul database mock

--- src/main/java/co/develhope/mockdb/entities/Student.java
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -10,9 +10,6 @@ import javax.persistence.*;
 
 @Table
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Student{
 
     @Id
@@ -24,4 +21,46 @@ public class Student{
 
     @Column(unique = true)
     private String email;
+
+    public Student() {
+    }
+
+    public Student(long id_studente, String nome, String cognome, String email) {
+        this.id_studente = id_studente;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+
+    public long getId_studente() {
+        return id_studente;
+    }
+
+    public void setId_studente(long id_studente) {
+        this.id_studente = id_studente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
